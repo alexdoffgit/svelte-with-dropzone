ac1086300472118758f2a18983f2d59d50358357
alexdoffgit <alexdoffacc@gmail.com>
2022-05-26 18:44:36 +0700

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+example of svelte action with [dropzonejs](https://dropzone.dev/js)
+
+run this app:
+```
+$ git clone https://github.com/alexdoffgit/svelte-with-dropzone
+$ cd svelte-with-dropzone
+$ npm install
+$ npm run dev
+```
---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Dropzone from "dropzone";
+
+	function dropzoneAction(node) {
+		let svd = new Dropzone(node, {
+			url: "#"
+		})
+	}
+</script>
+
+<div class="container">
+	<div use:dropzoneAction></div>
+</div>
+
+<style>
+	.container {
+		border: 1px solid black;
+		width: 300px;
+		height: 300px;
+	}
+</style>
